7b6d53204aaede701539dfe8ad2b99e595dad9a6
Victor Hurdugaci <victor.hurdugaci@microsoft.com>
2016-02-01 18:41:11 -0800

    Build with dotnet

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if ! [ -x "$(command -v mono)" ]; then
-  echo >&2 "Could not find 'mono' on the path."
-  exit 1
-fi
+buildFolder=.build
+koreBuildFolder=$buildFolder/KoreBuild-dotnet
+
+nugetPath=$buildFolder/nuget.exe
 
 if test `uname` = Darwin; then
     cachedir=~/Library/Caches/KBuild
@@ -16,50 +16,30 @@ else
 fi
 mkdir -p $cachedir
 nugetVersion=latest
-cachePath=$cachedir/nuget.$nugetVersion.exe
+cacheNuget=$cachedir/nuget.$nugetVersion.exe
 
-url=https://dist.nuget.org/win-x86-commandline/$nugetVersion/nuget.exe
+nugetUrl=https://dist.nuget.org/win-x86-commandline/$nugetVersion/nuget.exe
 
-if test ! -f $cachePath; then
-    wget -O $cachePath $url 2>/dev/null || curl -o $cachePath --location $url /dev/null
+if test ! -d $buildFolder; then
+    mkdir $buildFolder
 fi
 
-if test ! -e .nuget; then
-    mkdir .nuget
-    cp $cachePath .nuget/nuget.exe
-fi
-
-if test ! -d packages/Sake; then
-    mono .nuget/nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
-    mono .nuget/nuget.exe install Sake -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
-fi
-
-echo ""
-echo "Installing .NET Execution Environment..."
-echo ""
-
-if ! type dnvm > /dev/null 2>&1; then
-    source packages/KoreBuild/build/dnvm.sh
-fi
-
-if ! type dnx > /dev/null 2>&1 || [ -z "$SKIP_DNX_INSTALL" ]; then
-    dnvm install latest -runtime coreclr -alias default
-    if [ $? -ne 0 ]; then
-        echo >&2 ".NET Execution Environment installation has failed."
-        exit 1
+if test ! -f $nugetPath; then
+    if test ! -f $cacheNuget; then
+        wget -O $cacheNuget $nugetUrl 2>/dev/null || curl -o $cacheNuget --location $nugetUrl /dev/null
     fi
 
-    dnvm install default -runtime mono -alias default
-    if [ $? -ne 0 ]; then
-        echo >&2 ".NET Execution Environment installation has failed."
-        exit 1
-    fi
-else
-    dnvm use default -runtime mono
+    cp $cacheNuget $nugetPath
 fi
 
-echo ""
-echo "Building packages..."
-echo ""
+if test ! -d $koreBuildFolder; then
+    mono $nugetPath install KoreBuild-dotnet -ExcludeVersion -o $buildFolder -nocache -pre
+    chmod +x $koreBuildFolder/build/KoreBuild.sh
+fi
 
-mono packages/Sake/tools/Sake.exe -I packages/KoreBuild/build -f makefile.shade "$@"
+makeFile=makefile.shade
+if [ ! -e $makeFile ]; then
+    makeFile=$koreBuildFolder/build/makefile.shade
+fi
+
+./$koreBuildFolder/build/KoreBuild.sh -n $nugetPath -m $makeFile "$@"
